1353299b8a19d787b8175b9efe82555220f183c5
Tom Kew-Goodale <tom.kew-goodale@enhanceit.us>
2020-05-02 17:49:51 -0400

    Basic Register

---
+++ PolygonAttack/ViewControllers/AcRegisterViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AcRegisterViewController.swift
+//  PolygonAttack
+//
+//  Created by Field Employee on 5/2/20.
+//  Copyright © 2020 Field Employee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AcRegisterViewController: UIViewController {
+
+  @IBOutlet var NameTextField: UITextField!
+
+    @IBOutlet var PassTextField: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func register(_ sender: Any)
+    {
+        if NameTextField.text == "" || PassTextField.text == ""
+        {
+            let alert = UIAlertController(title: "Information", message: "Its Mandatory to enter all the fields", preferredStyle: .alert)
+
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+
+            alert.addAction(ok)
+            alert.addAction(cancel)
+
+            self.present(alert, animated: true, completion: nil)
+            return
+
+        }
+        
+        UserDefaults.standard.set(NameTextField.text, forKey: "username")
+        UserDefaults.standard.set(PassTextField.text, forKey: "password")
+        UserDefaults.standard.synchronize()
+        
+        let alert = UIAlertController(title: "Information", message: "Account Created successfully.", preferredStyle: .alert)
+
+        let ok = UIAlertAction(title: "Ok", style: .default){ action in
+            self.dismiss(animated: true)
+        }
+        alert.addAction(ok)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+
+        }
+    
+    @IBAction func ReturnButton(_ sender: Any) {
+        self.dismiss(animated: true)
+        return
+    }
+    
+    }
---
+++ PolygonAttack/ViewControllers/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  PolygonAttack
+//
+//  Created by Field Employee on 5/2/20.
+//  Copyright © 2020 Field Employee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var NameTextField: UITextField!
+    
+    @IBOutlet weak var PassTextField: UITextField!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func LoginButtonPress(_ sender: Any) {
+        
+        let username = NameTextField.text
+        let password = PassTextField.text
+        
+        if username == "" || password == "" {
+          let alert = UIAlertController(title: "Information", message: "Please enter all the fields", preferredStyle: .alert)
+
+            let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+
+            alert.addAction(ok)
+            alert.addAction(cancel)
+
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        doLogin (username!, password!)
+    }
+    
+    func doLogin (_ User: String,_ Pass:String){
+        
+    }
+    
+
+}
