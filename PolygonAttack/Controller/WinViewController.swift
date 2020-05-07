bedf3c705f1a0162d47d086b75e224ccd172fbbc
Tom Kew-Goodale <tom.kew-goodale@enhanceit.us>
2020-05-07 09:49:16 -0400

    basic win screen

---
+++ PolygonAttack/Controller/WinViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WinViewController.swift
+//  PolygonAttack
+//
+//  Created by Field Employee on 5/7/20.
+//  Copyright © 2020 Field Employee. All rights reserved.
+//
+
+import UIKit
+
+class WinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+    @IBAction func ReturnButton(_ sender: Any) {
+        let viewController = self.storyboard?.instantiateViewController(withIdentifier: "Main Page") as! ViewController
+
+            viewController.modalPresentationStyle = .fullScreen
+            self.present(viewController, animated: false)
+        }
+    
+    
+}
